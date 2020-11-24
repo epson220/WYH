89e6f0cb3b3257ffc6b31877b1e539699a9f50a7
epson220 <62466439+epson220@users.noreply.github.com>
2020-11-24 22:20:12 +0900

    firstCommit

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    users: [],
+  };
+
+  componentDidMount() {
+    fetch("/users")
+      .then((res) => res.json())
+      .then((users) => this.setState({ users }));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Users</h1>
+        <ul>
+          {this.state.users.map((user) => (
+            <li key={user.id}>{user.username}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
