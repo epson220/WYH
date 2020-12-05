ea53156882d5b3d4a3aa406d7c208cd22b51da89
epson220 <62466439+epson220@users.noreply.github.com>
2020-12-05 21:48:25 +0900

    axios post test

--- frontend/src/components/DetailBoard.js
+++ frontend/src/components/DetailBoard.js
@@ -2,30 +2,21 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const DetailBoard = ({ match }) => {
-  const { boardId } = match.params;
+  const { board_id } = match.params;
   const [Board, setBoard] = useState([]);
-
-  useEffect(async () => {
-    async function postData() {
-      console.log("react detailBoard axios post 호출1");
-      axios
-        .post("http://localhost:3001/getDetailBoard", { id: boardId })
-        .then(function (response) {
-          console.log(response);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-    postData();
-  });
+  console.dir(match.params);
+  console.log("board_id : " + board_id);
 
   useEffect(async () => {
     async function fetchData() {
-      console.log("react detailBoard axios post 호출2");
-      const result = await axios.post("http://localhost:3001/getDetailBoard");
-      console.log(result.data);
-      setBoard(result.data);
+      console.log("react detailBoard axios post 호출1");
+      let res = await axios.post("http://localhost:3001/getDetailBoard", {
+        id: board_id,
+      });
+      console.log("res : ");
+      console.dir(res);
+      setBoard(res.data);
+      console.log(Board);
     }
     fetchData();
   }, []);
