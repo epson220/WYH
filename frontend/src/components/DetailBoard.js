567e471e47c0267bc5a06b2120430eaa583168d2
epson220 <62466439+epson220@users.noreply.github.com>
2020-12-06 01:55:59 +0900

    get Board detail

--- frontend/src/components/DetailBoard.js
+++ frontend/src/components/DetailBoard.js
@@ -1,9 +1,20 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const DetailBoard = ({ match }) => {
   const { board_id } = match.params;
-  const [Board, setBoard] = useState([]);
+  const [Board, setBoard] = useState({
+    title: "",
+    content: "",
+    hobby: "",
+    picture: "",
+    writer: "",
+    created_at: "",
+    _id: "",
+  });
+  const [Imgurl, setImgurl] = useState("");
+
   console.dir(match.params);
   console.log("board_id : " + board_id);
 
@@ -17,18 +28,22 @@ const DetailBoard = ({ match }) => {
       console.dir(res);
       setBoard(res.data);
       console.log(Board);
+      setImgurl("http://localhost:3001/uploads/" + res.data.picture);
     }
     fetchData();
   }, []);
 
   return (
-    <div>
-      {Board.map((b) => (
-        <div>
-          <div>{b.title}</div> <div>{b.content}</div>
-        </div>
-      ))}
-    </div>
+    <>
+      <Link to="/profile">작성자 : {Board.writer}</Link>
+      <h3 style={{ width: "50%", border: "1px solid black" }}>{Board.title}</h3>
+      <div style={{ width: "80%", border: "1px solid black" }}>
+        {Board.content}
+      </div>
+      <div>
+        <img src={Imgurl} style={{ width: "600px", height: "600px" }}></img>
+      </div>
+    </>
   );
 };
 
